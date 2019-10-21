c0f3cee21be72fe835b909abda7091b509417e60
Niel JORDAAN <njordaan@c4r23s1.wethinkcode.co.za>
2019-10-21 12:15:52 +0200

    669

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public float rotateSpeed;
+
+    public GameObject selectedObject;
+    
+    private void Start()
+    {
+        selectedObject = null;
+    }
+    
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            if (Input.GetKey(KeyCode.Mouse0))
+            {
+                RotateObject();
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                SelectObject();
+            }
+        }
+    }
+
+    void SelectObject()
+    {
+        Ray ray;
+
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit, 100.0f))
+        {
+            selectedObject = hit.collider.gameObject;
+        }
+    }
+
+    void RotateObject()
+    {
+        float x;
+        float y;
+
+        x = Input.GetAxis("Mouse Y") * rotateSpeed * Time.deltaTime;
+        y = Input.GetAxis("Mouse X") * rotateSpeed * Time.deltaTime;
+        selectedObject.transform.rotation = Quaternion.Euler(selectedObject.transform.rotation.eulerAngles.x + x, selectedObject.transform.rotation.eulerAngles.y - y, 0.0f);
+    }
+}
